465e76b3193bbe0f493581ed8378215b2b5ec09e
oscar garay <ojcgv21@gmail.com>
2015-05-23 01:50:33 +0200

    rey y reina

---
+++ chess-app/src/main/java/com/XPeru/chess/King.java
@@ -0,0 +1,17 @@
+package com.XPeru.chess;
+
+public class King extends ChessPieceBase {
+
+	@Override
+	boolean canBeChecked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	boolean isSupportCastle() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ chess-app/src/main/java/com/XPeru/chess/Queen.java
@@ -0,0 +1,17 @@
+package com.XPeru.chess;
+
+public class Queen extends ChessPieceBase {
+
+	@Override
+	boolean canBeChecked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	boolean isSupportCastle() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
