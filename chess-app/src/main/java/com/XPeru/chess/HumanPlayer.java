c25bb91c2d841373328f0e47296ea70f27030b09
oscar garay <ojcgv21@gmail.com>
2015-05-23 01:35:21 +0200

    Class HumanPlayer

---
+++ chess-app/src/main/java/com/XPeru/chess/HumanPlayer.java
@@ -0,0 +1,11 @@
+package com.XPeru.chess;
+
+public class HumanPlayer extends PlayerBase {
+
+	@Override
+	public ChessPieceTurn getTurn(Position p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
