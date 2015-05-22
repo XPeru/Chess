8c36b5399f2c94f54d1beca3cbbfdb4fb21062db
oscar garay <ojcgv21@gmail.com>
2015-05-23 01:32:09 +0200

    Game Manager

---
+++ chess-app/src/main/java/com/XPeru/chess/GameManager.java
@@ -0,0 +1,13 @@
+package com.XPeru.chess;
+
+public class GameManager {
+	void processTurn(PlayerBase player) {
+		
+	}
+	
+	boolean acceptTurn(ChessPieceTurn turn) {
+		return true;
+	}
+	
+	Position currentPosition;
+}
